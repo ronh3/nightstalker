351ededa8c399527e614572689abb8d56f564965
Ron <rholcomb3@gmail.com>
2022-09-27 22:29:19 -0700

    Changed version number and disabled stylesheet.

--- build/tmp/config.lua
+++ build/tmp/config.lua
@@ -4,5 +4,5 @@ title = [[UI for Starmourn]]
 description = [[# nightstalker
 Githubbing Nightstalker
 ]]
-version = [[1.97]]
-created = [[2022-09-27T02:27:06+0000]]
+version = [[1.0]]
+created = [[2022-09-28T05:27:09+0000]]
